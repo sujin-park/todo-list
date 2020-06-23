ed7a342c4ebebb88b9fc5c39fabbc6ad54345486
sujin-park <psujin831@gmail.com>
2020-06-23 17:01:38 +0900

    [A] Drag & Drop library in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
+import GlobalStyle from './style/GlobalStyle';
 import Theme from './style/Theme';
+import Footer from './components/Footer';
+import TodoApp from './pages/TodoApp';
 
-const App: React.FC = () => (
+// drag & drop library
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
+const App: React.FC = () => {
+  return (
     <ThemeProvider theme={Theme}>
-      <BrowserRouter>
-      </BrowserRouter>
+      <DndProvider backend={HTML5Backend}>
+        <GlobalStyle />
+        <TodoApp />
+      </DndProvider>
+      <Footer />
     </ThemeProvider>
-);
+  );
+};
 
 export default App;
