5f95fd9dbc25c30e11ff86e126ff632b48bb4917
sujin-park <psujin831@gmail.com>
2020-06-23 11:27:09 +0900

    [U] GlobalStyle, Theme black color

--- src/style/Theme.ts
+++ src/style/Theme.ts
@@ -1,17 +1,15 @@
-const BOX_BORDER = '1px solid #dee2e6'
-const BORDER_RADIUS = '4px'
+const BOX_BORDER = '1px solid #dee2e6';
+const BORDER_RADIUS = '4px';
 
 export default {
   maxWidth: '100%',
   primaryColor: '#FF922B',
-  bgColor: '#FAFAFA',
-  blackColor: '#222',
-  darkGreyColor: '#999',
+  blackColor: '#000',
   lightGreyColor: '#D3D3D3',
   redColor: '#FF2264',
   boxBorder: BOX_BORDER,
   borderRadius: BORDER_RADIUS,
   whiteBox: `border-radius: ${BORDER_RADIUS};
              border: ${BOX_BORDER};
-             background-color: #fff`
-}
+             background-color: #fff`,
+};
